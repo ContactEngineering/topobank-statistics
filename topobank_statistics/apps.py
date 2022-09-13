419b7288035f0afab5d33ee3f05bb88f636fd3e8
Michael Röttger <info@michael-roettger.de>
2022-09-13 16:11:56 +0200

    WIP: Further steps to an installable package.
    Moved AppConfig to apps module for automatic discovery.

---
+++ topobank_statistics/apps.py
@@ -0,0 +1,39 @@
+import pkg_resources
+__version__ = pkg_resources.require("topobank-statistics")[0].version
+
+try:
+    from topobank.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use topobank 0.92.0 or above to use this plugin!")
+
+class StatisticsPluginConfig(PluginConfig):
+    default = True
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'topobank_statistics'
+    verbose_name = "Statistical Analysis"
+
+    class TopobankPluginMeta:
+        name = "Statistical Analysis"
+        version = __version__
+        description = """
+        Provides the following statistical analysis functions:
+        - Height/Curvature/Slope Distribution
+        - Autocorrelation
+        - Power Spectrum
+        - Roughness Parameters
+        - Scale-dependent slope/curvature
+        - Variable bandwidth
+        """
+        picture = "topobank_statistics/static/images/logo.svg"
+        compatibility = "topobank>=0.92.0"
+
+    def ready(self):
+        # make sure the functions are registered now
+        # TODO can this be done with signals?
+        # noinspection PyUnresolvedReferences
+        import topobank_statistics.functions
+        # noinspection PyUnresolvedReferences
+        import topobank_statistics.views
+        # noinspection PyUnresolvedReferences
+        import topobank_statistics.downloads
+
