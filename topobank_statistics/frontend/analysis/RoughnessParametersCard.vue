70dd144dca01e9abe72e6d6a0efb19a2fdf990aa
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-03-09 08:03:04 +0100

    WIP: Frontend code and webpack bundling for plugins

---
+++ topobank_statistics/frontend/analysis/RoughnessParametersCard.vue
@@ -0,0 +1,156 @@
+<script>
+
+import {v4 as uuid4} from 'uuid';
+import DataTable from 'datatables.net-vue3'
+
+import BibliographyModal from 'BibliographyModal.vue';
+import TasksButton from 'TasksButton.vue';
+
+export default {
+  name: 'series-card',
+  components: {
+    BibliographyModal,
+    BokehPlot,
+    DataTable
+  },
+  props: {
+    apiUrl: String,
+    csrfToken: String,
+    detailUrl: String,
+    enlargeButton: {
+      type: Boolean,
+      default: false
+    },
+    functionId: Number,
+    functionName: String,
+    subjects: Object,
+    txtDownloadUrl: String,
+    uid: {
+      type: String,
+      default() {
+        return uuid4();
+      }
+    },
+    xlsxDownloadUrl: String
+  },
+  data() {
+    return {
+      analyses: [],
+      analysesAvailable: false,
+      categories: undefined,
+      dataSources: undefined,
+      dois: [],
+      hasWarnings: false,
+      outputBackend: undefined,
+      plots: undefined,
+      title: this.functionName
+    }
+  },
+  mounted() {
+    this.updateCard();
+  },
+  methods: {
+    updateCard() {
+      /* Fetch JSON describing the card */
+      fetch(this.apiUrl, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-CSRFToken': this.csrfToken
+        },
+        body: JSON.stringify({
+          function_id: this.functionId,
+          subjects: this.subjects
+        })
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            this.analyses = data.analyses;
+            this.title = data.plotConfiguration.title;
+            this.plots = [{
+              title: "default",
+              xAxisLabel: data.plotConfiguration.xAxisLabel,
+              yAxisLabel: data.plotConfiguration.yAxisLabel,
+              xAxisType: data.plotConfiguration.xAxisType,
+              yAxisType: data.plotConfiguration.yAxisType
+            }];
+            this.dataSources = data.plotConfiguration.dataSources;
+            this.categories = data.plotConfiguration.categories;
+            this.outputBackend = data.plotConfiguration.outputBackend;
+            this.dois = data.dois;
+            this.analysesAvailable = true;
+          });
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="card search-result-card">
+    <div class="card-header">
+      <div class="btn-group btn-group-sm float-right">
+        <tasks-button :analyses="analyses"
+                      :csrf-token="csrfToken">
+        </tasks-button>
+        <button @click="updateCard" class="btn btn-default float-right ml-1">
+          <i class="fa fa-redo"></i>
+        </button>
+        <div v-if="enlargeButton" class="btn-group btn-group-sm float-right">
+          <a :href="detailUrl" class="btn btn-default float-right">
+            <i class="fa fa-expand"></i>
+          </a>
+        </div>
+      </div>
+      <a class="text-dark" href="#" data-toggle="collapse" :data-target="`#sidebar-${uid}`">
+        <h5><i class="fa fa-bars"></i> {{ title }}</h5>
+      </a>
+    </div>
+    <div class="card-body">
+      <div v-if="!analysesAvailable" class="tab-content">
+        <span class="spinner"></span>
+        <div>Please wait...</div>
+      </div>
+
+      <div v-if="analysesAvailable" class="tab-content">
+        <div class="tab-pane show active" role="tabpanel" aria-label="Tab showing a plot">
+          <data-table class="table table-striped table-bordered">
+            
+          </data-table>
+        </div>
+      </div>
+    </div>
+    <div :id="`sidebar-${uid}`" class="col-1 col-sm-6 p-0 collapse sidebar position-absolute h-100">
+      <!-- card-header sets the margins identical to the card so the title appears at the same position -->
+      <nav class="card-header navbar navbar-toggleable-xl bg-light flex-column align-items-start h-100">
+        <ul class="flex-column navbar-nav">
+          <a class="text-dark" href="#" data-toggle="collapse" :data-target="`#sidebar-${uid}`">
+            <h5><i class="fa fa-bars"></i> {{ title }}</h5>
+          </a>
+          <li class="nav-item">
+            Download
+            <a :href="txtDownloadUrl">
+              TXT
+            </a>
+            <a :href="xlsxDownloadUrl">
+              XLSX
+            </a>
+            <a v-on:click="$refs.plot.download()">
+              SVG
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" data-toggle="modal" :data-target="`#bibliography-modal-${uid}`">
+              Bibliography
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <bibliography-modal
+      :id="`bibliography-modal-${uid}`"
+      :dois="dois">
+  </bibliography-modal>
+</template>
