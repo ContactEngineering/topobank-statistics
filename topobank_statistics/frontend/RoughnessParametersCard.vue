3cceb6432061c088fea271f7314959014f26e789
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-05-07 17:41:49 +0200

    BUG: Show message if no results are returned

--- topobank_statistics/frontend/RoughnessParametersCard.vue
+++ topobank_statistics/frontend/RoughnessParametersCard.vue
@@ -32,7 +32,7 @@ export default {
         },
         functionId: Number,
         functionName: String,
-        subjects: Object,
+        subjects: String,
         txtDownloadUrl: String,
         uid: {
             type: String,
