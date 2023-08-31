fb53a6804e4a8fb0171fd831f19f5cbc13e19098
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-08-31 14:35:18 +0200

    Revert "MAINT: Optimize database access"
    This reverts commit 9bedded4485cde789ea7e63138be8689bc62063e.

--- topobank_statistics/views.py
+++ topobank_statistics/views.py
@@ -34,7 +34,7 @@ def roughness_parameters_card_view(request, **kwargs):
     #
     context = {
         'dois': controller.dois,
-        'analyses': controller.get_analysis_ids()
+        'analyses': controller.to_representation(request=request)
     }
 
     #
