d5938e9d7065b7d1fa39de866c6243f3306caed2
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-12-15 21:54:30 +0100

    TST: Added API test

---
+++ topobank_statistics/tests/test_api.py
@@ -0,0 +1,5 @@
+from django.urls import reverse
+
+def test_api():
+    """Test API routes"""
+    assert reverse('topobank_statistics:card-roughness-parameters', kwargs=dict(function_id=123)) == '/plugins/statistics/card/roughness-parameters/123'
