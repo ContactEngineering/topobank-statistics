788a3389014196af028a4be5c826ec2dbfc55ac4
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2023-12-19 21:10:20 +0100

    MAINT: Return API URL from `get_absolute_url` of manager models

--- topobank_statistics/tests/urls.py
+++ topobank_statistics/tests/urls.py
@@ -6,21 +6,19 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from ce_ui.views import HomeView
+urlpatterns = [
+    path(
+        "manager/",
+        include("topobank.manager.urls", namespace="manager"),
+    ),
+    path(
+        "users/",
+        include("topobank.users.urls", namespace="users"),
+    ),
+    path(
+        "analysis/",
+        include("topobank.analysis.urls", namespace="analysis"),
+    ),
+]
 
-urlpatterns = [path("", HomeView.as_view(), name="home"),
-               path(
-                   "manager/",
-                   include("topobank.manager.urls", namespace="manager"),
-               ),
-               path(
-                   "users/",
-                   include("topobank.users.urls", namespace="users"),
-               ),
-               path(
-                   "analysis/",
-                   include("topobank.analysis.urls", namespace="analysis"),
-               ),
-               ] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
