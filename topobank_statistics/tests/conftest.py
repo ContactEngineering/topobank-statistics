8b926ff13b016e9facaf02056224c9f0901b89b2
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-01-18 22:31:12 +0100

    TST: Need to sync analysis functions

--- topobank_statistics/tests/conftest.py
+++ topobank_statistics/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from topobank.fixtures import api_rf, handle_usage_statistics  # noqa: F401
+from topobank.fixtures import api_rf, handle_usage_statistics, sync_analysis_functions  # noqa: F401
 from topobank.manager.tests.utils import two_topos  # noqa: F401
 from topobank.organizations.tests.utils import OrganizationFactory
 from topobank.users.tests.factories import UserFactory
@@ -8,7 +8,7 @@ from topobank.users.tests.factories import UserFactory
 
 @pytest.mark.django_db
 @pytest.fixture
-def user_with_plugin():
+def user_with_plugin(sync_analysis_functions):
     org_name = "Test Organization"
     org = OrganizationFactory(name=org_name, plugins_available="topobank_statistics")
     user = UserFactory()
