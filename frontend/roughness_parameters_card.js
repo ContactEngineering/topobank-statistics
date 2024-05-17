2edd9116325a8b426e84c58031716926e6efd850
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-05-17 22:12:26 +0200

    MAINT: Removed unused JS

--- frontend/roughness_parameters_card.js
+++
@@ -1,9 +0,0 @@
-import {createApp} from 'vue';
-import RoughnessParametersCard from './RoughnessParametersCard.vue';
-
-export function createCardApp(el, csrfToken, props) {
-    let app = createApp(RoughnessParametersCard, props);
-    app.provide('csrfToken', csrfToken);
-    app.mount(el);
-    return app;
-}
