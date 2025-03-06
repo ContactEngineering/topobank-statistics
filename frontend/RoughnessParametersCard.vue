38ae1f6ef245c308a9d869dab70bca9b2c073b50
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-03-06 20:59:48 +0100

    MAINT: Passing subjects as object

--- frontend/RoughnessParametersCard.vue
+++ frontend/RoughnessParametersCard.vue
@@ -1,33 +1,47 @@
 <script setup>
 
 import axios from "axios";
-import {computed, onMounted, ref} from "vue";
+import { computed, onMounted, ref } from "vue";
 
-import {BDropdownDivider, BDropdownItem} from "bootstrap-vue-next";
+import { BDropdownDivider, BDropdownItem, useToastController } from "bootstrap-vue-next";
 
-import DataTable from 'datatables.net-vue3';
-import DataTablesLib from 'datatables.net-bs5';
+import DataTable from "datatables.net-vue3";
+import DataTablesLib from "datatables.net-bs5";
 
 DataTable.use(DataTablesLib);
 
-import {formatExponential} from "topobank/utils/formatting";
+import { formatExponential } from "topobank/utils/formatting";
+import { subjectsToBase64 } from "topobank/utils/api";
 
 import AnalysisCard from "topobank/analysis/AnalysisCard.vue";
+
+const {show} = useToastController();
 
 const props = defineProps({
     apiUrl: {
         type: String,
-        default: '/plugins/statistics/card/roughness-parameters'
+        default: "/plugins/statistics/card/roughness-parameters"
     },
     detailUrl: {
         type: String,
-        default: '/ui/html/analysis-detail/'
+        default: "/ui/html/analysis-detail/"
     },
-    functionId: Number,
-    functionName: String,
-    subjects: String,
-    txtDownloadUrl: String,
-    xlsxDownloadUrl: String
+    enlarged: {
+        type: Boolean,
+        default: true
+    },
+    functionId: {
+        type: Number,
+        required: true
+    },
+    functionName: {
+        type: String,
+        required: true
+    },
+    subjects: {
+        type: Object,
+        required: true
+    }
 });
 
 // Displayed data
@@ -35,32 +49,34 @@ const _analyses = ref(null);
 const _columnDefs = ref([
     // Indicate that first column contains HTML
     // to have HTML tags removed for sorting/filtering
-    {targets: 0, type: 'html'}
+    { targets: 0, type: "html" }
 ]);
 const _columns = ref([
     {
-        title: 'Measurement',
-        render: function (data, type, row) {
+        title: "Measurement",
+        render: function(data, type, row) {
             let name = row.topography_name;
             return `<a target="_blank" title="${name}" href="${row.topography_url}">${name}</a>`;
         }
     },
-    {data: 'quantity', title: 'Quantity'},
-    {data: 'from', title: 'From'},
-    {data: 'symbol', title: '<span title="Symbol according to ASME B46.1">Sym. 🛈</span>'},
-    {data: 'direction', title: 'Direct.'},
+    { data: "quantity", title: "Quantity" },
+    { data: "from", title: "From" },
+    { data: "symbol", title: "<span title=\"Symbol according to ASME B46.1\">Sym. 🛈</span>" },
+    { data: "direction", title: "Direct." },
     {
-        data: 'value', title: 'Value', render: function (x) {
+        data: "value", title: "Value", render: function(x) {
             return formatExponential(x, 5);
         }
     },
-    {data: 'unit', title: 'Unit'},
+    { data: "unit", title: "Unit" }
 ]);
 const _dois = ref([]);
 const _data = ref([]);
+const _messages = ref([]);
 
 // GUI logic
 const _title = "Roughness parameters";
+const _nbPendingAjaxRequests = ref(0);
 
 onMounted(() => {
     updateCard();
@@ -72,19 +88,34 @@ const analysisIds = computed(() => {
 
 function updateCard() {
     /* Fetch JSON describing the card */
-    axios.get(`${props.apiUrl}/${props.functionId}?subjects=${props.subjects}`).then(response => {
-        _analyses.value = response.data.analyses;
-        /** replace null in value with NaN
-         * This is needed because we cannot pass NaN through JSON without
-         * extra libraries, so it is passed as null (workaround) */
-        _data.value = response.data.tableData.map(x => {
-            if (x['value'] === null) {
-                x['value'] = NaN;
-            }
-            return x
+    _nbPendingAjaxRequests.value++;
+    axios.get(`${props.apiUrl}/${props.functionId}?subjects=${subjectsToBase64(props.subjects)}`)
+        .then(response => {
+            _analyses.value = response.data.analyses;
+            /** replace null in value with NaN
+             * This is needed because we cannot pass NaN through JSON without
+             * extra libraries, so it is passed as null (workaround) */
+            _data.value = response.data.tableData.map(x => {
+                if (x["value"] === null) {
+                    x["value"] = NaN;
+                }
+                return x;
+            });
+            _dois.value = response.data.dois;
+            _messages.value = response.data.messages;
+        })
+        .catch(error => {
+            show?.({
+                props: {
+                    title: "Error fetching roughness parameters",
+                    body: error.message,
+                    variant: "danger"
+                }
+            });
+        })
+        .finally(() => {
+            _nbPendingAjaxRequests.value--;
         });
-        _dois.value = response.data.dois;
-    });
 }
 
 </script>
@@ -94,14 +125,14 @@ function updateCard() {
                   :detailUrl="detailUrl"
                   :dois="_dois"
                   :enlarged="enlarged"
-                  :messages="_messages"
                   :functionId="functionId"
-                  :subjects="subjects"
+                  :messages="_messages"
                   :showLoadingSpinner="_nbPendingAjaxRequests > 0"
+                  :subjects="subjects"
                   :title="_title"
                   @allTasksFinished="updateCard"
-                  @someTasksFinished="updateCard"
-                  @refreshButtonClicked="updateCard">
+                  @refreshButtonClicked="updateCard"
+                  @someTasksFinished="updateCard">
         <template #dropdowns>
             <BDropdownDivider v-if="_analyses != null && _analyses.length > 0"></BDropdownDivider>
             <BDropdownItem v-for="analysis in _analyses"
@@ -113,12 +144,12 @@ function updateCard() {
                 Download XLSX
             </BDropdownItem>
         </template>
-        <DataTable class="table table-striped table-bordered"
-                    :column-defs="_columnDefs"
-                    :columns="_columns"
-                    :data="_data"
-                    responsive="yes"
-                    scroll-x="yes">
+        <DataTable :column-defs="_columnDefs"
+                   :columns="_columns"
+                   :data="_data"
+                   class="table table-striped table-bordered"
+                   responsive="yes"
+                   scroll-x="yes">
         </DataTable>
     </AnalysisCard>
 </template>
