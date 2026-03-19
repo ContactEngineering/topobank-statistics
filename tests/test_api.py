094b667d75a010da1e41f5cc8b39f263f6a9b864
Lars Pastewka <lars.pastewka@surface.design>
2026-03-19 20:51:20 +0100

    MAINT: Renamed app config

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,8 @@
+import pytest
 from django.urls import reverse
 
 
+@pytest.mark.urls("test_urls")
 def test_api():
     """Test API routes"""
     assert (
--- topobank_statistics/apps.py
+++ topobank_statistics/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class StatisticsAppConfig(AppConfig):
+class TopobankStatisticsAppConfig(AppConfig):
     default = True
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'topobank_statistics'
